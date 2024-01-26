65c1fc518514d2158ec12cb55fa0f0b99fb178a9
yonikal56 <yonikal56@gmail.com>
2024-01-26 14:15:12 +0200

    start implementing int to bag conversion

--- src/preprocessing/passes/int_to_bag.cpp
+++ src/preprocessing/passes/int_to_bag.cpp
@@ -44,6 +44,43 @@ namespace cvc5::internal {
 namespace preprocessing {
 namespace passes {
 
+Node convertIntToBag(int n) {
+    std::vector<Node> children;
+
+    // Print the number of 2s that divide n
+    while (n % 2 == 0)
+    {
+      children.push_back(NodeManager::currentNM()->mkConstInt(Rational(2)));
+      n = n/2;
+    }
+
+    // n must be odd at this point. So we can skip
+    // one element (Note i = i +2)
+    for (int i = 3; i <= std::sqrt(n); i = i + 2)
+    {
+      // While i divides n, print i and divide n
+      while (n % i == 0)
+      {
+        children.push_back(NodeManager::currentNM()->mkConstInt(Rational(i)));
+        n = n/i;
+      }
+    }
+
+    // This condition is to handle the case when n
+    // is a prime number greater than 2
+    if (n > 2)
+      children.push_back(NodeManager::currentNM()->mkConstInt(Rational(n)));
+
+  Node node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, children.at(1));
+  int size = children.size();
+  for (int j = 1; j < size; j++) {
+    node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, node, children.at(j));
+  }
+
+
+    return node;
+}
+
 IntToBag::IntToBag(PreprocessingPassContext* preprocContext)
    : PreprocessingPass(preprocContext, "int-to-bag"),
      d_funcToSkolem(userContext()),
@@ -55,16 +92,12 @@ IntToBag::IntToBag(PreprocessingPassContext* preprocContext)
 PreprocessingPassResult IntToBag::applyInternal(
    AssertionPipeline* assertionsToPreprocess)
 {
+  Node bag1 = convertIntToBag(315);
+  Node bag2 = convertIntToBag(19);
+  Node res = NodeManager::currentNM()->mkNode(Kind::BAG_UNION_DISJOINT, bag1, bag2);
 
- for (unsigned i = 0; i < assertionsToPreprocess->size(); ++i)
+  for (unsigned i = 0; i < assertionsToPreprocess->size(); ++i)
  {
-   Node int1 = NodeManager::currentNM()->mkConstInt(Rational(5));
-   Node int2 = NodeManager::currentNM()->mkConstInt(Rational(3));
-   Node int0 = NodeManager::currentNM()->mkConstInt(Rational(1));
-   Node bag1 = NodeManager::currentNM()->mkNode(Kind::BAG_MAKE, int2, int0);
-   Node bag2 = NodeManager::currentNM()->mkNode(Kind::BAG_MAKE, int1, int0);
-   Node res = NodeManager::currentNM()->mkNode(Kind::BAG_UNION_DISJOINT, bag1, bag2);
-   Node result = NodeManager::currentNM()->mkConst(false);
    assertionsToPreprocess->replace(
        i, rewrite(res));
  }
