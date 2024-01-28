b12efba49fcae524504184525a32e26cf9e2f5f4
yonikal56 <yonikal56@gmail.com>
2024-01-28 12:04:53 +0200

    start converting assertions into real bags

--- src/preprocessing/passes/int_to_bag.cpp
+++ src/preprocessing/passes/int_to_bag.cpp
@@ -1,106 +1,163 @@
 /******************************************************************************
-* Top contributors (to current version):
-*   Ying Sheng, Yoni Zohar, Aina Niemetz
-*
-* This file is part of the cvc5 project.
-*
-* Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
-* in the top-level source directory and their institutional affiliations.
-* All rights reserved.  See the file COPYING in the top-level source
-* directory for licensing information.
-* ****************************************************************************
-*
-* Ackermannization preprocessing pass.
-*
-* This implements the Ackermannization preprocessing pass, which enables
-* very limited theory combination support for eager bit-blasting via
-* Ackermannization. It reduces constraints over the combination of the
-* theories of fixed-size bit-vectors and uninterpreted functions as
-* described in
-*   Liana Hadarean, An Efficient and Trustworthy Theory Solver for
-*   Bit-vectors in Satisfiability Modulo Theories.
-*   https://cs.nyu.edu/media/publications/hadarean_liana.pdf
-*/
+ * Top contributors (to current version):
+ *   Ying Sheng, Yoni Zohar, Aina Niemetz
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Ackermannization preprocessing pass.
+ *
+ * This implements the Ackermannization preprocessing pass, which enables
+ * very limited theory combination support for eager bit-blasting via
+ * Ackermannization. It reduces constraints over the combination of the
+ * theories of fixed-size bit-vectors and uninterpreted functions as
+ * described in
+ *   Liana Hadarean, An Efficient and Trustworthy Theory Solver for
+ *   Bit-vectors in Satisfiability Modulo Theories.
+ *   https://cs.nyu.edu/media/publications/hadarean_liana.pdf
+ */
 
 #include "preprocessing/passes/int_to_bag.h"
 
 #include <cmath>
 
 #include "base/check.h"
+#include "expr/node.h"
 #include "expr/node_algorithm.h"
+#include "expr/node_traversal.h"
 #include "expr/skolem_manager.h"
-#include "smt/logic_exception.h"
 #include "options/base_options.h"
 #include "options/options.h"
-#include "expr/node.h"
 #include "preprocessing/assertion_pipeline.h"
 #include "preprocessing/preprocessing_pass_context.h"
+#include "smt/logic_exception.h"
+#include "theory/rewriter.h"
+#include "theory/theory.h"
 #include "util/rational.h"
 
 using namespace cvc5::internal;
 using namespace cvc5::internal::theory;
 
 namespace cvc5::internal {
-    namespace preprocessing {
-        namespace passes {
+namespace preprocessing {
+namespace passes {
 
-            Node convertIntToBag(int n) {
-                std::vector <Node> children;
+using namespace std;
+using namespace cvc5::internal::theory;
 
-                // Print the number of 2s that divide n
-                while (n % 2 == 0) {
-                    children.push_back(NodeManager::currentNM()->mkConstInt(Rational(2)));
-                    n = n / 2;
-                }
+Node convertAssertion(TNode n, NodeMap& cache)
+{
+  for (TNode current :
+       NodeDfsIterable(n, VisitOrder::POSTORDER, [&cache](TNode nn) {
+         return cache.count(nn) > 0;
+       }))
+  {
+    Node result;
+    //std::cout<<toString(current.getKind())<<current.toString()<<to_string(current.getNumChildren())<<std::endl;
+    NodeManager* nm = NodeManager::currentNM();
+    if (current.getNumChildren() == 0)
+    {
+      result = current;
+    }
+    else if (current.getNumChildren() == 2
+             && (current.getKind() == Kind::MULT))
+    {
+      Assert(cache.find(current[0]) != cache.end());
+      result = cache[current[0]];
+      for (unsigned i = 1; i < current.getNumChildren(); ++i)
+      {
+        Assert(cache.find(current[i]) != cache.end());
+        Node child = current[i];
+        Node childRes = cache[current[i]];
+        result = nm->mkNode(Kind::BAG_UNION_DISJOINT, result, childRes);
+      }
+    }
+    else
+    {
+      NodeBuilder builder(current.getKind());
+      if (current.getMetaKind() == kind::metakind::PARAMETERIZED)
+      {
+        builder << current.getOperator();
+      }
 
-                // n must be odd at this point. So we can skip
-                // one element (Note i = i +2)
-                for (int i = 3; i <= std::sqrt(n); i = i + 2) {
-                    // While i divides n, print i and divide n
-                    while (n % i == 0) {
-                        children.push_back(NodeManager::currentNM()->mkConstInt(Rational(i)));
-                        n = n / i;
-                    }
-                }
+      for (unsigned i = 0; i < current.getNumChildren(); ++i)
+      {
+        Assert(cache.find(current[i]) != cache.end());
+        builder << cache[current[i]];
+      }
+      result = builder;
+    }
+    cache[current] = result;
+  }
+  return cache[n];
+}
 
-                // This condition is to handle the case when n
-                // is a prime number greater than 2
-                if (n > 2)
-                    children.push_back(NodeManager::currentNM()->mkConstInt(Rational(n)));
+Node convertIntToBag(int n)
+{
+  /*std::vector <Node> children;
+  children.push_back(NodeManager::currentNM()->mkNode(Kind::SET_UNIVERSE));
 
-                Node node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, children.at(1));
-                int size = children.size();
-                for (int j = 1; j < size; j++) {
-                    node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, node, children.at(j));
-                }
+  // Print the number of 2s that divide n
+  while (n % 2 == 0) {
+      children.push_back(NodeManager::currentNM()->mkConstInt(Rational(2)));
+      n = n / 2;
+  }
 
+  // n must be odd at this point. So we can skip
+  // one element (Note i = i +2)
+  for (int i = 3; i <= std::sqrt(n); i = i + 2) {
+      // While i divides n, print i and divide n
+      while (n % i == 0) {
+          children.push_back(NodeManager::currentNM()->mkConstInt(Rational(i)));
+          n = n / i;
+      }
+  }
 
-                return node;
-            }
+  // This condition is to handle the case when n
+  // is a prime number greater than 2
+  if (n > 2)
+      children.push_back(NodeManager::currentNM()->mkConstInt(Rational(n)));
 
-            IntToBag::IntToBag(PreprocessingPassContext *preprocContext)
-                    : PreprocessingPass(preprocContext, "int-to-bag"),
-                      d_funcToSkolem(userContext()),
-                      d_usVarsToBVVars(userContext()),
-                      d_logic(logicInfo()) {
-            }
+  Node node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, children);*/
+  return NodeManager::currentNM()->mkNode(
+      Kind::BAG_MAKE,
+      {NodeManager::currentNM()->mkConstInt(Rational(n)),
+       NodeManager::currentNM()->mkConstInt(Rational(1))});
+}
 
-            PreprocessingPassResult IntToBag::applyInternal(
-                    AssertionPipeline *assertionsToPreprocess) {
-                Node bag1 = convertIntToBag(315);
-                Node bag2 = convertIntToBag(19);
-                Node res = NodeManager::currentNM()->mkNode(Kind::BAG_UNION_DISJOINT, bag1, bag2);
+IntToBag::IntToBag(PreprocessingPassContext* preprocContext)
+    : PreprocessingPass(preprocContext, "int-to-bag"),
+      d_funcToSkolem(userContext()),
+      d_usVarsToBVVars(userContext()),
+      d_logic(logicInfo())
+{
+}
 
-                for (unsigned i = 0; i < assertionsToPreprocess->size(); ++i) {
-                    assertionsToPreprocess->replace(
-                            i, rewrite(res));
-                }
+PreprocessingPassResult IntToBag::applyInternal(
+    AssertionPipeline* assertionsToPreprocess)
+{
+  Node bag1 = convertIntToBag(315);
+  Node bag2 = convertIntToBag(19);
+  Node res =
+      NodeManager::currentNM()->mkNode(Kind::BAG_UNION_DISJOINT, bag1, bag2);
 
-                return PreprocessingPassResult::NO_CONFLICT;
-            }
+  NodeMap cache;
+  for (unsigned i = 0; i < assertionsToPreprocess->size(); ++i)
+  {
+    assertionsToPreprocess->replace(
+        i, convertAssertion((*assertionsToPreprocess)[i], cache));
+  }
+
+  return PreprocessingPassResult::NO_CONFLICT;
+}
 
 /* -------------------------------------------------------------------------- */
 
-        }  // namespace passes
-    }  // namespace preprocessing
+}  // namespace passes
+}  // namespace preprocessing
 }  // namespace cvc5::internal
--- src/preprocessing/passes/int_to_bag.h
+++ src/preprocessing/passes/int_to_bag.h
@@ -1,26 +1,26 @@
 /******************************************************************************
-* Top contributors (to current version):
-*   Aina Niemetz, Ying Sheng, Mathias Preiner
-*
-* This file is part of the cvc5 project.
-*
-* Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
-* in the top-level source directory and their institutional affiliations.
-* All rights reserved.  See the file COPYING in the top-level source
-* directory for licensing information.
-* ****************************************************************************
-*
-* Ackermannization preprocessing pass.
-*
-* This implements the Ackermannization preprocessing pass, which enables
-* very limited theory combination support for eager bit-blasting via
-* Ackermannization. It reduces constraints over the combination of the
-* theories of fixed-size bit-vectors and uninterpreted functions as
-* described in
-*   Liana Hadarean, An Efficient and Trustworthy Theory Solver for
-*   Bit-vectors in Satisfiability Modulo Theories.
-*   https://cs.nyu.edu/media/publications/hadarean_liana.pdf
-*/
+ * Top contributors (to current version):
+ *   Aina Niemetz, Ying Sheng, Mathias Preiner
+ *
+ * This file is part of the cvc5 project.
+ *
+ * Copyright (c) 2009-2023 by the authors listed in the file AUTHORS
+ * in the top-level source directory and their institutional affiliations.
+ * All rights reserved.  See the file COPYING in the top-level source
+ * directory for licensing information.
+ * ****************************************************************************
+ *
+ * Ackermannization preprocessing pass.
+ *
+ * This implements the Ackermannization preprocessing pass, which enables
+ * very limited theory combination support for eager bit-blasting via
+ * Ackermannization. It reduces constraints over the combination of the
+ * theories of fixed-size bit-vectors and uninterpreted functions as
+ * described in
+ *   Liana Hadarean, An Efficient and Trustworthy Theory Solver for
+ *   Bit-vectors in Satisfiability Modulo Theories.
+ *   https://cs.nyu.edu/media/publications/hadarean_liana.pdf
+ */
 
 #include "cvc5_private.h"
 
@@ -37,32 +37,32 @@
 namespace cvc5::internal {
 namespace preprocessing {
 namespace passes {
-
+using NodeMap = std::unordered_map<Node, Node>;
 using TNodeSet = std::unordered_set<TNode>;
 using FunctionToArgsMap = std::unordered_map<TNode, TNodeSet>;
 using USortToBVSizeMap = std::unordered_map<TypeNode, size_t>;
 
 class IntToBag : public PreprocessingPass
 {
-public:
- IntToBag(PreprocessingPassContext* preprocContext);
+ public:
+  IntToBag(PreprocessingPassContext* preprocContext);
 
-protected:
- PreprocessingPassResult applyInternal(
-     AssertionPipeline* assertionsToPreprocess) override;
+ protected:
+  PreprocessingPassResult applyInternal(
+      AssertionPipeline* assertionsToPreprocess) override;
 
-private:
- /* Map each function to a set of terms associated with it */
- FunctionToArgsMap d_funcToArgs;
- /* Map each function-application term to the new Skolem variable created by
-  * ackermannization */
- theory::SubstitutionMap d_funcToSkolem;
- /* Map each variable with uninterpreted sort to the new Skolem BV variable
-  * created by ackermannization */
- theory::SubstitutionMap d_usVarsToBVVars;
- /* Map each uninterpreted sort to the number of variables in this sort. */
- USortToBVSizeMap d_usortCardinality;
- LogicInfo d_logic;
+ private:
+  /* Map each function to a set of terms associated with it */
+  FunctionToArgsMap d_funcToArgs;
+  /* Map each function-application term to the new Skolem variable created by
+   * ackermannization */
+  theory::SubstitutionMap d_funcToSkolem;
+  /* Map each variable with uninterpreted sort to the new Skolem BV variable
+   * created by ackermannization */
+  theory::SubstitutionMap d_usVarsToBVVars;
+  /* Map each uninterpreted sort to the number of variables in this sort. */
+  USortToBVSizeMap d_usortCardinality;
+  LogicInfo d_logic;
 };
 
 }  // namespace passes
