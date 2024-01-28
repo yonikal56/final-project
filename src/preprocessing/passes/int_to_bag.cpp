127ad2ccd93e08889fe300c9018e67c9cd5d6e54
yonikal56 <yonikal56@gmail.com>
2024-01-28 10:17:06 +0200

    refactoring

--- src/preprocessing/passes/int_to_bag.cpp
+++ src/preprocessing/passes/int_to_bag.cpp
@@ -41,72 +41,66 @@ using namespace cvc5::internal;
 using namespace cvc5::internal::theory;
 
 namespace cvc5::internal {
-namespace preprocessing {
-namespace passes {
+    namespace preprocessing {
+        namespace passes {
 
-Node convertIntToBag(int n) {
-    std::vector<Node> children;
+            Node convertIntToBag(int n) {
+                std::vector <Node> children;
 
-    // Print the number of 2s that divide n
-    while (n % 2 == 0)
-    {
-      children.push_back(NodeManager::currentNM()->mkConstInt(Rational(2)));
-      n = n/2;
-    }
+                // Print the number of 2s that divide n
+                while (n % 2 == 0) {
+                    children.push_back(NodeManager::currentNM()->mkConstInt(Rational(2)));
+                    n = n / 2;
+                }
 
-    // n must be odd at this point. So we can skip
-    // one element (Note i = i +2)
-    for (int i = 3; i <= std::sqrt(n); i = i + 2)
-    {
-      // While i divides n, print i and divide n
-      while (n % i == 0)
-      {
-        children.push_back(NodeManager::currentNM()->mkConstInt(Rational(i)));
-        n = n/i;
-      }
-    }
+                // n must be odd at this point. So we can skip
+                // one element (Note i = i +2)
+                for (int i = 3; i <= std::sqrt(n); i = i + 2) {
+                    // While i divides n, print i and divide n
+                    while (n % i == 0) {
+                        children.push_back(NodeManager::currentNM()->mkConstInt(Rational(i)));
+                        n = n / i;
+                    }
+                }
 
-    // This condition is to handle the case when n
-    // is a prime number greater than 2
-    if (n > 2)
-      children.push_back(NodeManager::currentNM()->mkConstInt(Rational(n)));
+                // This condition is to handle the case when n
+                // is a prime number greater than 2
+                if (n > 2)
+                    children.push_back(NodeManager::currentNM()->mkConstInt(Rational(n)));
 
-  Node node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, children.at(1));
-  int size = children.size();
-  for (int j = 1; j < size; j++) {
-    node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, node, children.at(j));
-  }
+                Node node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, children.at(1));
+                int size = children.size();
+                for (int j = 1; j < size; j++) {
+                    node = NodeManager::currentNM()->mkNode(Kind::SET_INSERT, node, children.at(j));
+                }
 
 
-    return node;
-}
+                return node;
+            }
 
-IntToBag::IntToBag(PreprocessingPassContext* preprocContext)
-   : PreprocessingPass(preprocContext, "int-to-bag"),
-     d_funcToSkolem(userContext()),
-     d_usVarsToBVVars(userContext()),
-     d_logic(logicInfo())
-{
-}
+            IntToBag::IntToBag(PreprocessingPassContext *preprocContext)
+                    : PreprocessingPass(preprocContext, "int-to-bag"),
+                      d_funcToSkolem(userContext()),
+                      d_usVarsToBVVars(userContext()),
+                      d_logic(logicInfo()) {
+            }
 
-PreprocessingPassResult IntToBag::applyInternal(
-   AssertionPipeline* assertionsToPreprocess)
-{
-  Node bag1 = convertIntToBag(315);
-  Node bag2 = convertIntToBag(19);
-  Node res = NodeManager::currentNM()->mkNode(Kind::BAG_UNION_DISJOINT, bag1, bag2);
+            PreprocessingPassResult IntToBag::applyInternal(
+                    AssertionPipeline *assertionsToPreprocess) {
+                Node bag1 = convertIntToBag(315);
+                Node bag2 = convertIntToBag(19);
+                Node res = NodeManager::currentNM()->mkNode(Kind::BAG_UNION_DISJOINT, bag1, bag2);
 
-  for (unsigned i = 0; i < assertionsToPreprocess->size(); ++i)
- {
-   assertionsToPreprocess->replace(
-       i, rewrite(res));
- }
+                for (unsigned i = 0; i < assertionsToPreprocess->size(); ++i) {
+                    assertionsToPreprocess->replace(
+                            i, rewrite(res));
+                }
 
- return PreprocessingPassResult::NO_CONFLICT;
-}
+                return PreprocessingPassResult::NO_CONFLICT;
+            }
 
 /* -------------------------------------------------------------------------- */
 
-}  // namespace passes
-}  // namespace preprocessing
+        }  // namespace passes
+    }  // namespace preprocessing
 }  // namespace cvc5::internal
