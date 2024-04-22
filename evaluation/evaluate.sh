72e6f966315da6549fe692ee8a39eee714842b9c
yonikal56 <yonikal56@gmail.com>
2024-04-22 18:22:50 +0300

    update evaluation script to count number of fails/timeouts with and without the new implementation

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+withFails=0
+withoutFails=0
+
 for filename in ./intToBag/**/*.smt2; do
-  echo "$filename result using solveIntAsBag:"
-  timeout 10 ../build/bin/cvc5 --solve-int-as-bag $filename
-  echo "$filename result without solveIntAsBag:"
-  timeout 10 ../build/bin/cvc5 $filename
+  timeout 10 ../build/bin/cvc5 --solve-int-as-bag $filename > /dev/null 2>&1
+  RESULT=$?
+  if [ $RESULT -ne 0 ]; then
+    echo "$filename using solveIntAsBag failed"
+    ((withFails++))
+  fi
+  timeout 10 ../build/bin/cvc5 $filename > /dev/null 2>&1
+  RESULT=$?
+  if [ $RESULT -ne 0 ]; then
+    echo "$filename without solveIntAsBag failed"
+    ((withoutFails++))
+  fi
 done
+
+echo "solveIntAsBag fails: $withFails"
+echo "without solveIntAsBag fails: $withoutFails"
