a7afe1978c42d41ea2a612705d7ece416727aec0
yonikal56 <yonikal56@gmail.com>
2024-06-24 13:51:51 +0300

    tests creation

--- evaluation/evaluate.sh
+++ evaluation/evaluate.sh
@@ -9,12 +9,14 @@ for filename in ./intToBag/**/*.smt2; do
   RESULT=$?
   if [ $RESULT -ne 0 ]; then
     echo "$filename using solveIntAsBag failed"
+    echo "$filename" >> "solveIntAsBagFails.txt"
     ((withFails++))
   fi
   timeout 10 ../build/bin/cvc5 $filename > /dev/null 2>&1
   RESULT=$?
   if [ $RESULT -ne 0 ]; then
     echo "$filename without solveIntAsBag failed"
+    echo "$filename" >> "withoutSolveIntAsBagFails.txt"
     ((withoutFails++))
   fi
 done
